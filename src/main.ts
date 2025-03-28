50cfef171c99149e962678ee132befc39408fa5f
Александр "axel_verse" Мудров <alexander@mudrov.tech>
2025-03-28 14:31:35 +0300

    Changes - 2

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
-export default async function ({req, res, log, error}: any) {
+export default async ({req, res}: any) =>{
     if (req.method !== 'GET') return res.status(405).json({ message: 'Method not allowed' });
-
-    log('Function works!');
-    error('This is an error!');
 
     if (req.path === '/ping') return res.json({ message: 'pong' });
 
-    return res.status(404).json({ message: 'Not found' });
+    return res.json({ message: 'Hello world' });
 }
