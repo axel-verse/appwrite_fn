f3f35d1362bb985106277bde4f689145e5dd8ea0
Александр "axel_verse" Мудров <alexander@mudrov.tech>
2025-03-28 14:57:55 +0300

    Changes - 3

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
-export default async ({req, res}: any) =>{
+import { Client, Users } from 'node-appwrite';
+
+export default async ({req, res, log, error}: any) =>{
     if (req.method !== 'GET') return res.status(405).json({ message: 'Method not allowed' });
 
-    if (req.path === '/ping') return res.json({ message: 'pong' });
+    const client = new Client()
+        .setEndpoint(<string>Bun.env["APPWRITE_FUNCTION_API_ENDPOINT"])
+        .setProject(<string>Bun.env["APPWRITE_FUNCTION_PROJECT_ID"])
+        .setKey(req.headers['x-appwrite-key'] ?? '');
+    const users = new Users(client);
+
+    try {
+        const response = await users.list();
+        log(`Total users: ${response.total}`);
+    } catch (err) {
+        // @ts-ignore
+        error(`Could not fetch users: ${err.message}`);
+    }
+
+    if (req.path === '/ping') return res.text('pong');
 
     return res.json({ message: 'Hello world' });
 }
