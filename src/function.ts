909c67546708af67fd6964b09a9a63185455bb62
Александр "axel_verse" Мудров <alexander@mudrov.tech>
2025-03-28 11:02:00 +0300

    Test function

---
+++ src/function.ts
@@ -0,0 +1,10 @@
+export default function ({req, res, log, error}: any) {
+    if (req.method !== 'GET') return res.status(405).json({ message: 'Method not allowed' });
+
+    log('Function works!');
+    error('This is an error!');
+
+    if (req.path === '/ping') return res.json({ message: 'pong' });
+
+    return res.status(404).json({ message: 'Not found' });
+}
